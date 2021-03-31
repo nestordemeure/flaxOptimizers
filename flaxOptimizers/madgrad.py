8c0e6760dacb745edbe2ebe59f8bcebb8fb0cfc6
Nestor Demeure <nestordemeure@gmail.com>
2021-03-31 20:35:51 +0200

    added weight decay and remapped learning rate to make it linear

--- flaxOptimizers/madgrad.py
+++ flaxOptimizers/madgrad.py
@@ -10,6 +10,7 @@ class _MadgradHyperParams:
     beta: onp.ndarray
     eps: onp.ndarray
     weight_decay: onp.ndarray
+    use_adamWStyle_weightDecay: onp.bool
 
 @struct.dataclass
 class _MadgradParamState:
@@ -22,18 +23,22 @@ class Madgrad(OptimizerDef):
     MADGRAD: Momentumized, Adaptive, Dual Averaged Gradient Method for Stochastic Optimization.
     https://github.com/facebookresearch/madgrad
 
+    Differences with reference implementation:
+    - this version uses a AdamW style weight decay by default
+    - the learning rate has been remapped such that it acts linearly
+
     Notes:
-    - Madgrad usually requires less weight decay than other optimizer (often 0).
+    - Madgrad with use_adamWStyle_weightDecay=False usually requires less weight decay than other optimizer (often 0).
     - The optimal learning rate will usually not be the one used by SGD or Adam.
       Some good values include :
-      - 2.5e-4 for 152 layer PreActResNet on CIFAR10,
-      - 0.001 for ResNet-50 on ImageNet,
-      - 0.025 for IWSLT14 using transformer_iwslt_de_en,
-      - 0.005 for RoBERTa training on BookWiki using BERT_BASE.
+      - 0.005 for 152 layer PreActResNet on CIFAR10,
+      - 0.01 for ResNet-50 on ImageNet,
+      - 0.085 for IWSLT14 using transformer_iwslt_de_en,
+      - 0.03 for RoBERTa training on BookWiki using BERT_BASE.
     """
 
-    def __init__(self, learning_rate=1e-2, beta=0.9, eps=1e-6, weight_decay=0.0):
-        hyper_params = _MadgradHyperParams(learning_rate, beta, eps, weight_decay)
+    def __init__(self, learning_rate=5e-2, beta=0.9, eps=1e-6, weight_decay=0.0, use_adamWStyle_weightDecay=True):
+        hyper_params = _MadgradHyperParams(learning_rate, beta, eps, weight_decay, use_adamWStyle_weightDecay)
         super().__init__(hyper_params)
 
     def init_param_state(self, param):
@@ -46,16 +51,22 @@ class Madgrad(OptimizerDef):
         eps = hyper_params.eps
 
         # weight decay
-        grad += weight_decay * param
+        if not hyper_params.use_adamWStyle_weightDecay:
+            grad += weight_decay * param
 
         # gradient accumulation
-        weighted_lr = learning_rate * jnp.sqrt(step + 1)
+        # power(3/2) added such that learning_rate is the actual step size rather than lr / cbrt(lr)
+        weighted_lr = jnp.power(learning_rate, 3./2.) * jnp.sqrt(step + 1)
         grad_sum = state.grad_sum + weighted_lr * grad
         grad_sum_sq = state.grad_sum_sq + weighted_lr * lax.square(grad)
 
         # parameter update
         new_param = state.initial_param - grad_sum / (jnp.cbrt(grad_sum_sq) + eps)
         new_param = beta*param + (1. - beta)*new_param # momentum
+
+        # AdamW-style weight decay
+        if hyper_params.use_adamWStyle_weightDecay:
+            new_param -= (1. - beta) * learning_rate * weight_decay * param
 
         new_state = _MadgradParamState(state.initial_param, grad_sum, grad_sum_sq)
         return new_param, new_state
